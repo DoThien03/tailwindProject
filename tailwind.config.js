2592359e67be9c1f03b66008c0de1a18e853b8b4
thien <dothien.dvt.2508@gmail.com>
2023-09-12 16:38:57 +0700

    responsive_header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./publicc/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        karla: ['Karla', ' sans-serif']
+      },
+      colors: {
+        'light-coffee': '#C89F94'
+      },
+      keyframes: {
+        slideDown: {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0)' },
+        }
+      },
+      animation: {
+        slideDown: 'slideDown 0.4s ease-in-out',
+      }
+    },
   },
   plugins: [],
 }
